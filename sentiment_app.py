d92e7a61e7a55c05be9d6909eaff13ed52326bda
ankitsharma5911 <ankitsharma450306@gmail.com>
2024-11-26 20:19:17 +0530

    first commit

--- sentiment_app.py
+++ sentiment_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
 from transformers import pipeline
 
-st.title()
+import streamlit as st
+from transformers import pipeline
 
 
+st.title("sentiment Analysis App")
+
+st.write("Welcome to my app")
+
+
+def load_pipeline():
+    return pipeline(task="text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
+
+sentiment_pipeline = load_pipeline()
+user_input = st.text_area("Enter text")
+
+if st.button("Analyze Sentiment"):
+    if user_input.strip():
+        result = sentiment_pipeline(user_input)
+        sentiment = result[0]["label"]
+        confidence = result[0]["score"]
+        st.write(f"Sentiment: {sentiment}")
+        st.write(f"Confidence: {confidence:.2f}%")
+    else :
+        st.write("Please enter some text")
